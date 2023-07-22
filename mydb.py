a6ca315f81d5262848e2c11bf39f1c8d5d36bf41
Pramod <57028365+Pramod025@users.noreply.github.com>
2023-07-22 21:40:54 +0530

    Update mydb.py

--- mydb.py
+++ mydb.py
@@ -3,7 +3,7 @@ import mysql.connector
 dataBase=mysql.connector.connect(
     host='localhost',
     user='root',
-    passwd='Password@SQL25'
+    passwd='***********'
 )
 
  #prepare a cursor object
